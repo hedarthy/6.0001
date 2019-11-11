094600b3da53cc5758cf840c9d109972b98e570c
flaminger <freakytire@gmail.com>
2019-11-10 17:24:27 -0800

    cleaned second attempt, still getting incorrect numbers

--- ps1a.py
+++ ps1a.py
@@ -1,24 +1,26 @@
 #part 1a
 
-#take the inputs
-annual_salary = int(input("Enter your annual salary: "))
+#ask user for salary, percent of salary saved, and total cost of home
+annual_salary = float(input("Enter your annual salary: "))
 portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
 total_cost = float(input("Enter the cost of your dream home: "))
 
+#data from problem
 portion_down_payment = 0.25
-current_savings = 0
-r = 0.04
-counter = 0
-
-#calculate how much you need to save
 cost_downpayment = total_cost * portion_down_payment
+r = 0.04
+monthly_salary = annual_salary / 12
+monthly_return = r / 12
+monthly_deposits = portion_saved * monthly_return
+current_savings = 0.0
 
-#calculate how much you save on a monthly basis
-monthly_earnings = current_savings * r/12
+months = 0
 
 while current_savings < cost_downpayment:
-    monthly_savings = (annual_salary / 12) * portion_saved
-    current_savings = (current_savings*r/12) + monthly_savings
-    counter += 1
+    months += 1 #count the months
+    
+    #multiply savings by rate of return
+    current_savings *= 1 + monthly_return
+    current_savings += monthly_deposits
 
-print("Number of months: ", counter)
+print("Months: ", months)
