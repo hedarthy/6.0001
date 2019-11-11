726216961dcfdd61c942608691f945bb1f70e006
flaminger <freakytire@gmail.com>
2019-11-10 18:12:52 -0800

    second attempt success

--- ps1b.py
+++ ps1b.py
@@ -4,6 +4,7 @@
 annual_salary = float(input("Enter your annual salary: "))
 portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
 total_cost = float(input("Enter the cost of your dream home: "))
+semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal: "))
 
 #data from problem
 portion_down_payment = 0.25
@@ -23,6 +24,11 @@ current_savings = 0.0
 
 while current_savings < cost_downpayment:
     months += 1
+
+    if months % 6 == 0:
+        annual_salary = annual_salary * (1 + semi_annual_raise)
+        monthly_salary = annual_salary / 12.0
+        monthly_deposit = monthly_salary * portion_saved
 
     returnOnInvestment = (monthly_r)*current_savings
     current_savings += returnOnInvestment
