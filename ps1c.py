129f80f823e2358bdf886746a7103e945e85e66b
Hedarthy <hedarthycodes@gmail.com>
2019-11-18 00:15:28 -0800

    gave up for now will come back later, working code

--- ps1c.py
+++ ps1c.py
@@ -1,57 +1,53 @@
-#part 1c
+#ps1c
+#modified code from @tuthang102
 
-#ask user for salary, and fill in the blanks
+#user input
 annual_salary = float(input("Enter the starting salary: "))
+placeholder = annual_salary
+
+#problem data
 total_cost = 1000000
-semi_annual_raise = 0.07
-
-#data from problem
-annual_r = 0.04
-monthly_r = annual_r / 12.0
-
-#how much to add monthly
-monthly_salary = annual_salary / 12.0
-#monthly_deposit = monthly_salary #* portion_saved guess
-
-#cost of home
-cost_downpayment = 0.25 * total_cost
-
-#counter and savings
+semi_raise = 0.07
+current_savings = 0
+portion_down_payment = 0.25 * total_cost
 months = 36
-current_savings = 0.0
 
-#bisection sort 
+#acceptable range and bisection search
 epsilon = 100
-num_guesses = 0
 low = 0
 high = 10000
-guess = ((low + high) / 2.0)
+counter = 0
 
-while abs(current_savings - cost_downpayment) >= epsilon:
-
+while True:
     #reset
+    portion_saved = (low + high) / 2
+    annual_salary = placeholder
     current_savings = 0
+    monthly_salary = annual_salary / 12.0
 
     for i in range(1, months + 1):
-        monthly_deposit = monthly_salary * (guess / 10000)
+        returnOnInvestment = (current_savings * 0.04) / 12
+        monthlyDeposit = float(monthly_salary * portion_saved) / 10000
+        current_savings = current_savings + monthlyDeposit + returnOnInvestment
 
         if i % 6 == 0:
-            annual_salary = annual_salary * (1 + semi_annual_raise)
-            monthly_salary = annual_salary / 12.0
-            monthly_deposit = monthly_salary * (guess / 10000)
+            annual_salary = annual_salary + (annual_salary + semi_raise)
+    
+    if abs(current_savings - portion_down_payment) <= epsilon:
+        print("Best savings rate: ", round(portion_saved / 100, 2), "%")
+        print("Steps in binary search", counter)
+        break
 
-        returnOnInvestment = monthly_r * current_savings
-        current_savings += returnOnInvestment
-        current_savings += monthly_deposit
+    #if guess was too high
+    elif abs(current_savings - portion_down_payment) > epsilon and current_savings > portion_down_payment:
+        high = portion_saved
 
-    if current_savings > cost_downpayment:
-        #look in upper half
-        low = guess
-    else:
-        #look in lower half
-        high = guess
-    guess = (low + high)/2.0
-    num_guesses += 1 #counts guesses
-
-print("best savings rate:", guess)
-print("Stepts in bisection search: ", num_guesses)
+    #if guess was too low
+    elif abs(current_savings - portion_down_payment) > epsilon and current_savings < portion_down_payment:
+        low = portion_saved
+    
+    if low == high:
+        print("It is not possible to pay the downpayment in three years.")
+        break
+    
+    counter += 1
